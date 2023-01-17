cfd49b51af23514f72635836589f7a7864e7a38a
Nikhil Chawla <nikhil1916.cse18@chitkara.edu.in>
2023-01-17 19:34:45 +0530

    added demo stripe payment api with dummy data

--- Routers/bookingRouter.js
+++ Routers/bookingRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bookingRouter = express.Router();
 const { protectRoute } = require('../helper');
 const { createSession } = require('../controller/bookingController');
+bookingRouter.use(express.static("public"));
 
 bookingRouter.route('/createSession').get(function (req, res) {
-  res.sendFile("F:/FoodApp/Booking.html");
+  res.sendFile("F:/FoodApp/public/Booking.html");
 });
 
-bookingRouter.use(protectRoute);
-bookingRouter.route('/createSession/:id').post(createSession);
+// bookingRouter.use(protectRoute);
+bookingRouter.route('/createSession').post(createSession);
 
 module.exports = bookingRouter;
--- controller/bookingController.js
+++ controller/bookingController.js
@@ -2,37 +2,48 @@ let SK = "sk_test_51MMmJmSE0D9IClxDVLDf8OR2N91MD8GlOuqimIhvMA0PViEia3RbIM7CtMn15
 const stripe = require('stripe')(SK);
 const planModel = require('../models/planModel');
 const userModel = require('../models/userModel');
-
 module.exports.createSession = async function (req, res) {
   console.log("create session");
   try {
-    const userId = req.id;
-    const planId = req.params.id;
-    const user = await userModel.findById(userId);
-    const plan = await planModel.findById(planId);
+    // const userId = req.id;
+    // const planId = req.params.id;
+    // const user = await userModel.findById(userId);
+    // const plan = await planModel.findById(planId);
+    const product = await stripe.products.create({
+      name: 'T-shirt',
+      description: 'Comfortable cotton t-shirt',
+      images: ['https://example.com/t-shirt.png'],
+    });
+
+    const price = await stripe.prices.create({
+      product: product.id,
+      unit_amount: 2000,
+      currency: 'usd',
+    });
+
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      customer_email: user.email,
-      client_reference_id: plan.id,
+      // payment_method_types: ['card'],
+      // customer_email: user.email,
+      // client_reference_id: plan.id,
       line_items: [
         {
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-          name: plan.name,
-          description: plan.description,
-          price: 700,
-          quantity: 1,
-          amount: plan.price * 100,
-          currency: 'inr',
+          // name: "health plan", //plan.name
+          // // description: plan.description,
+          // // price: 700,
+          // // quantity: 1,
+          // amount: 1234, //plan.price * 100,
+          // // currency: 'inr',
+          // quantity: 1,
+          price: price.id,
           quantity: 1,
         },
       ],
+      mode: "payment",
       success_url: `${req.protocol}://${req.get("host")}/profile`,
       cancel_url: `${req.protocol}://${req.get("host")}/profile`,
     });
-    res.json({
-      msg: "success",
-      session
-    });
+    res.redirect(303, session.url);
   } catch (err) {
     console.log(err.message);
     res.json({
